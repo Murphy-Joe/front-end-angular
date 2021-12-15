c9b6003318fedb0f49905f0946512fad903b7dc0
murphy-joe <ibidnsell@yahoo.com>
2021-12-15 15:46:55 -0500

    added an event emitter to parent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { GiftGivingComponent } from './components/gift-giving/gift-giving.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { HeaderComponent } from './components/header/header.component'
+import { GiftGivingComponent } from './components/gift-giving/gift-giving.component'
+import { DashboardComponent } from './components/dashboard/dashboard.component'
+import { NavigationComponent } from './components/navigation/navigation.component'
+import { GiftEntryComponent } from './components/gift-giving/gift-entry/gift-entry.component'
+import { GiftListComponent } from './components/gift-giving/gift-list/gift-list.component'
+import { ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -14,13 +17,12 @@ import { NavigationComponent } from './components/navigation/navigation.componen
     HeaderComponent,
     GiftGivingComponent,
     DashboardComponent,
-    NavigationComponent
+    NavigationComponent,
+    GiftEntryComponent,
+    GiftListComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/gift-giving/gift-list/gift-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core'
+import { GiftListItemModel } from '../../models'
+
+@Component({
+  selector: 'app-gift-list',
+  templateUrl: './gift-list.component.html',
+  styleUrls: ['./gift-list.component.css'],
+})
+export class GiftListComponent {
+  @Input()
+  giftList: GiftListItemModel[] = []
+  markPurchased(item: GiftListItemModel) {
+    console.log(`They purchased ${item.description}`)
+  }
+
+  constructor() {}
+}
